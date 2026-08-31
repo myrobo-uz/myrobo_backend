2c5e6c7e67470f4c7b1974c6826a7b39be79d240
izzatillo <adxamovizzatillo008@gmail.com>
2026-08-31 10:21:49 +0500

    fix permisson

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from rest_framework.parsers import FormParser, MultiPartParser
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import serializers, status
@@ -21,7 +21,7 @@ class ContactSerializer(serializers.ModelSerializer):
 
 
 class ContactView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     parser_classes = [MultiPartParser, FormParser]
 
     def post(self, request):
