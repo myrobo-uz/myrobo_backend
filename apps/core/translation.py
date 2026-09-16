a6ec9fcef6758d2c51bf8ec935e688cac65d7e94
izzatillo <adxamovizzatillo008@gmail.com>
2026-09-17 01:22:34 +0500

    contact

--- apps/core/translation.py
+++
@@ -1,8 +0,0 @@
-from modeltranslation.translator import register, TranslationOptions
-
-from .models import Contact
-
-
-@register(Contact)
-class ContactTranslationOptions(TranslationOptions):
-    fields = ("name", "message")
